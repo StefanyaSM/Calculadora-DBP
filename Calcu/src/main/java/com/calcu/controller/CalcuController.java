1dd0fdd185252c49c9f1f8a3a3917030f1eb1474
StefanyaSM <stefanyasalm@gmail.com>
2026-02-05 18:56:27 -0600

    Abre /calcu

---
+++ Calcu/src/main/java/com/calcu/controller/CalcuController.java
@@ -0,0 +1,69 @@
+package com.calcu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//TAREA
+//CALCULADORA CON @PathVariable, @RequestParam y @RequestBody
+
+@RestController
+public class CalcuController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "Hola";
+    }
+
+    @PostMapping("/prod")
+    public String createProduct(@RequestBody Product product) {
+        return "Producto recibido: " + product.getName();
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello " + name;
+    }
+    
+    
+    @GetMapping("/calcu")
+    public String calcuHome() {
+    	return "Bienvenido a una calculadora bien basica";
+    }
+
+    @GetMapping("/calcu/{oper}/{n1}/{n2}")
+    public String calc(@PathVariable String oper,
+                     
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+        return "Resultado = " + resultado;
+    }
+}
+  
---
+++ Calcu/src/main/java/com/calcu/controller/Product.java
@@ -0,0 +1,14 @@
+package com.calcu.controller;
+public class Product {
+
+    private String name;
+    //get y set, y ya
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
